b40131dcb453a728a9f2f4f3b356a1fee0807758
Andrija Đurić <andrija.djur@gmail.com>
2025-03-11 20:49:58 +0100

    init upload

---
+++ _next/static/K4qVsjW0dXbrvqI6SgpnC/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/303ffe72268190e1.css","static/chunks/pages/index-d9daa4b875941cdd.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
